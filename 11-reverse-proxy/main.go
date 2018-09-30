2e3f14b6e7e40b712cce8e7ca57ec3be81bfb353
acoshift <acoshift@gmail.com>
2018-09-30 12:48:11 +0700

    add reverse proxy

---
+++ 11-reverse-proxy/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	srv := reverseProxy{
+		Listen: 9000,
+		Locations: []location{
+			{"/", "https://www.google.com"},
+			{"/superman", "https://example.com"},
+		},
+	}
+	log.Fatal(srv.Start())
+}
+
+type location struct {
+	Path   string
+	Target string
+}
+
+type reverseProxy struct {
+	*http.ServeMux
+
+	Listen    int
+	Locations []location
+}
+
+func (p *reverseProxy) Start() error {
+	// setup
+	p.ServeMux = http.NewServeMux()
+
+	for _, l := range p.Locations {
+		p.ServeMux.Handle(l.Path, p.makeReverseProxy(l.Target))
+	}
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", p.Listen), p)
+}
+
+func (reverseProxy) makeReverseProxy(target string) http.Handler {
+	targetURL, _ := url.Parse(target)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Scheme = targetURL.Scheme
+		r.URL.Host = targetURL.Host
+		r.Host = targetURL.Host
+
+		resp, err := http.DefaultTransport.RoundTrip(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusServiceUnavailable)
+			return
+		}
+		for k, v := range resp.Header {
+			for _, vv := range v {
+				w.Header().Add(k, vv)
+			}
+		}
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
+	})
+}
