f65d59e8c0757081fcfdafec9fc8db217567b061
acoshift <acoshift@gmail.com>
2018-10-03 18:12:14 +0700

    update load balancer example

--- 14-load-balancer/main.go
+++ 14-load-balancer/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
-	"net/url"
 	"sync/atomic"
 )
 
+func startMockServer(addr string) {
+	http.ListenAndServe(addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Serve from %s", addr)
+	}))
+}
+
 func main() {
 	servers := []string{
-		"https://www.google.com",
-		"https://acourse.io",
-		"https://github.com",
-		"https://www.nekopost.net",
+		"127.0.0.1:8001",
+		"127.0.0.1:8002",
+		"127.0.0.1:8003",
+		"127.0.0.1:8004",
+		"127.0.0.1:8005",
+	}
+
+	for _, addr := range servers {
+		go startMockServer(addr)
 	}
 
 	var index int64
@@ -20,20 +31,16 @@ func main() {
 	p := &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			// round-robin
-
-			i := atomic.LoadInt64(&index)
-			i++
+			i := atomic.AddInt64(&index, 1)
 			if i >= int64(len(servers)) {
+				atomic.StoreInt64(&index, 0)
 				i = 0
 			}
-			atomic.StoreInt64(&index, i)
 
-			target := servers[i]
-			targetURL, _ := url.Parse(target)
+			upstream := servers[i]
 
-			r.URL.Host = targetURL.Host
-			r.URL.Scheme = targetURL.Scheme
-			r.Host = targetURL.Host
+			r.URL.Host = upstream
+			r.URL.Scheme = "http"
 		},
 	}
 	http.ListenAndServe(":9000", p)
