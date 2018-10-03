575eab7b72928a94ef740b17aa0ede890114fd0a
acoshift <acoshift@gmail.com>
2018-10-03 17:38:42 +0700

    update hash example

--- 02-hashing/main.go
+++ 02-hashing/main.go
@@ -3,19 +3,46 @@ package main
 import (
 	"crypto/sha1"
 	"fmt"
-	"io/ioutil"
 	"math/big"
 )
 
-func main() {
-	data, _ := ioutil.ReadFile("text.txt")
-
-	sum := new(big.Int)
-	for _, p := range data {
-		sum.Add(sum, new(big.Int).SetInt64(int64(p)))
+func sum(b []byte) []byte {
+	s := new(big.Int)
+	for _, p := range b {
+		s.Add(s, new(big.Int).SetInt64(int64(p)))
 	}
-	fmt.Printf("sum: %x\n", sum.Bytes())
+	return s.Bytes()
+}
 
-	p := sha1.Sum(data)
-	fmt.Printf("sha1: %x\n", p)
+func djb2(b []byte) uint64 {
+	var hash uint64 = 5381
+	for _, c := range b {
+		hash = hash<<5 + hash + uint64(c)
+	}
+	return hash
+}
+
+func main() {
+	data1 := "I do love you, don't I"
+	data2 := "I don't love you, do I"
+	fmt.Println("data1:", data1)
+	fmt.Println("data2:", data2)
+
+	sum1 := sum([]byte(data1))
+	fmt.Printf("sum data1: %x\n", sum1)
+
+	sum2 := sum([]byte(data2))
+	fmt.Printf("sum data2: %x\n", sum2) // hash collision
+
+	djb21 := djb2([]byte(data1))
+	fmt.Printf("djb2 data1: %x\n", djb21)
+
+	djb22 := djb2([]byte(data2))
+	fmt.Printf("djb2 data1: %x\n", djb22)
+
+	sha11 := sha1.Sum([]byte(data1))
+	fmt.Printf("sha1 data1: %x\n", sha11)
+
+	sha12 := sha1.Sum([]byte(data2))
+	fmt.Printf("sha1 data2: %x\n", sha12)
 }
