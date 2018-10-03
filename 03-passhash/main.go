e6bf95baea029016264a97c81f5846b331c83237
acoshift <acoshift@gmail.com>
2018-10-03 17:57:10 +0700

    update passhash example

--- 03-passhash/main.go
+++ 03-passhash/main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/base64"
 	"fmt"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+// don't use in production
+func hashPassword(password string) string {
+	// generate salt
+	salt := make([]byte, 16)
+	rand.Read(salt)
+
+	// hash password with salt
+	h := sha256.New()
+	h.Write(salt)
+	hashed := h.Sum([]byte(password))
+
+	hashedPassword := append([]byte{}, salt...)
+	hashedPassword = append(hashedPassword, hashed...)
+	return base64.RawStdEncoding.EncodeToString(hashedPassword)
+}
+
+func comparePassword(hashedPassword string, password string) bool {
+	rawHashedPassword, _ := base64.RawStdEncoding.DecodeString(hashedPassword)
+
+	salt := rawHashedPassword[:16]
+	hashed := rawHashedPassword[16:]
+
+	h := sha256.New()
+	h.Write(salt)
+	hashedNew := h.Sum([]byte(password))
+
+	return subtle.ConstantTimeCompare(hashed, hashedNew) == 1
+}
+
 func main() {
 	pass := "superman"
+	fmt.Println("password:", pass)
 
-	// encrypt OrpheanBeholderScryDoubt with pass + salt
-	hash, _ := bcrypt.GenerateFromPassword([]byte(pass), 10)
+	fmt.Println("---- custom algor ----")
 
-	err := bcrypt.CompareHashAndPassword(hash, []byte("superman"))
-	if err != nil {
-		fmt.Println("pass not match")
-		return
-	}
-	fmt.Println("pass matched")
+	hashedCustom := hashPassword(pass)
+	fmt.Println("hashed password:", hashedCustom)
+
+	fmt.Println("compare with 'supersaiyan':", comparePassword(hashedCustom, "supersaiyan"))
+	fmt.Println("compare with 'superman':", comparePassword(hashedCustom, "superman"))
+
+	// bcrypt
+	fmt.Println("---- bcrypt ----")
+
+	// encrypt OrpheanBeholderScryDoubt using blowfish with pass + salt as key
+	hashedBcrypt, _ := bcrypt.GenerateFromPassword([]byte(pass), 10)
+	fmt.Printf("hashed password: %s\n", hashedBcrypt)
+
+	fmt.Println("compare with 'supersaiyan':", bcrypt.CompareHashAndPassword(hashedBcrypt, []byte("supersaiyan")) == nil)
+	fmt.Println("compare with 'superman':", bcrypt.CompareHashAndPassword(hashedBcrypt, []byte("superman")) == nil)
 }
